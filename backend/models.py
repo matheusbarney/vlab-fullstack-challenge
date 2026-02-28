aa1a6c6611d5dcfeafde7d8e7ff7142e5e3fc742
matheusbarney <matheusbarneygalindo@gmail.com>
2026-02-28 18:01:59 -0300

    chore: Initial linter adjustments

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,10 @@
 from database import Base
 from sqlalchemy import Column, Integer, String
 
+
 class Material(Base):
     __tablename__ = "material"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
     description = Column(String(999), nullable=False)
+    
